e0559bb9812cbaeeddd7806a9e746fb5245bccac
Ryu hyunmin <rhm8934@gmail.com>
2018-06-18 15:04:28 +0900

    아키텍쳐 설계 1. diary 테이블 생성 2. controller, service, dao 작성 3. mybatis-config, mapper 작성 4. webjar dependency 작성 5. main.html 작성

---
+++ src/main/java/com/inc/dayary/service/DiaryServiceImpl.java
@@ -0,0 +1,20 @@
+package com.inc.dayary.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.inc.dayary.domain.Diary;
+import com.inc.dayary.repositoy.DiaryDao;
+
+@Service
+public class DiaryServiceImpl implements DiaryService {
+	@Autowired
+	private DiaryDao diaryDao;
+
+	@Override
+	public List<Diary> list() {
+		return diaryDao.list();
+	}
+}
