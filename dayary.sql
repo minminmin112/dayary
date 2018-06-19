bd7345511cd33aedfa8e3929a81c0052e3864e5f
Ryu hyunmin <rhm8934@gmail.com>
2018-06-19 11:33:38 +0900

    수정 : 데이터베이스 주소

---
+++ dayary.sql
@@ -0,0 +1,21 @@
+create user mins identified by 11111111;
+
+grant dba to mins;
+
+create table diary(
+    id number primary key,
+    u_id varchar2(15),
+    title varchar2(50),
+    content clob not null,
+    regdate date not null
+);
+
+create sequence seq_diary_id;
+
+insert into diary values(seq_diary_id.nextval, 'ryu', '첫 번째 일기', '곧 점심시간이다.', sysdate);
+
+insert into diary values(seq_diary_id.nextval, 'ryu', '두 번째 일기', '곧 프로젝트를 시작해야 한다.', sysdate);
+
+commit;
+
+select * from diary;
